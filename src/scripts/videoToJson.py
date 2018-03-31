a41d186ec7bf3b3b6232cc908f79f5431902edd4
Ali Hashemi <alihashe@usc.edu>
2018-03-31 15:19:02 -0700

    Command line arguments instead of hardcoded variables, temp file to store intermediate data, json to stdout instead of saving to file

--- src/scripts/videoToJson.py
+++ src/scripts/videoToJson.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import moviepy.editor as mp
 import os
 import cv2
@@ -5,59 +6,61 @@ from PIL import Image
 import json
 import re
 import sys
+import shutil
 
-originalVideoFile = "/Users/alihashemi/Desktop/video.mp4"
-resizedVideoFile = "/Users/alihashemi/Desktop/resized.mp4"
-framesDirectory = "/Users/alihashemi/Desktop/frames"
-jsonOutputFile = "/Users/alihashemi/Desktop/json.json"
-height = 36
-width = 48
+originalVideoFile = sys.argv[1]
+height = int(sys.argv[2])
+width = int(sys.argv[3])
+resizedVideoFile = sys.path[0] + "/temp/resized.mp4"
+framesDirectory = sys.path[0] + "/temp/frames"
 
 def main():
-    resize(48, 36, originalVideoFile, resizedVideoFile)
-    saveFrames(resizedVideoFile, framesDirectory)
-    frames = generateList(width, height, framesDirectory)
-    saveJson(jsonOutputFile, frames)
+    prep()
+    resize(originalVideoFile)
+    saveFrames(resizedVideoFile)
+    frames = generateList()
+    outputJson(frames)
+    clean()
 
 # Resize the video to the given dimensions and save
-def resize(w, h, videoFile, resizedVideoFile):
+def resize(videoFile):
     clip = mp.VideoFileClip(videoFile)
-    clip_resized = clip.resize(newsize=(w, h))
+    clip_resized = clip.resize(newsize=(width, height))
     clip_resized.write_videofile(resizedVideoFile)
-    print ("Finished Rezising the video")
+    eprint ("Finished Rezising the video")
 
 # Save all the frames of a given video as png files into the given directory name
-def saveFrames(videoFileName, framesDirectory):
+def saveFrames(videoFileName):
     if not os.path.exists(framesDirectory):
         os.makedirs(framesDirectory)
 
-    print ("Saving frames of video...")
+    eprint ("Saving frames of video...")
     video = cv2.VideoCapture(videoFileName)
     didRead, frame = video.read()
     count = 0;
     didRead = True
     while (didRead):
-        cv2.imwrite(framesDirectory + "/Frame{}.png".format(count), frame)
+        cv2.imwrite(framesDirectory + "/frame{}.png".format(count), frame)
         didRead, frame = video.read()
         count+=1
 
 # Go through a file containing all the frames and create a list in the following format
 # list[row in frame][column in frame][frame number]
-def generateList(w, h, framesDirectory):
+def generateList():
     if not os.path.exists(framesDirectory):
-        print ("Error: {} does not exist".format(framesDirectory))
+        eprint ("Error: {} does not exist".format(framesDirectory))
         return
 
     # Initialize frames list
     frames = []
-    for i in xrange(h):
+    for i in xrange(height):
         row = []
-        for j in xrange(w):
+        for j in xrange(width):
             colors = []
             row.append(colors)
         frames.append(row)
 
-    print ("Generating list of pixel colors...")
+    eprint ("Generating list of pixel colors...")
     for fil in sort(os.listdir(framesDirectory)):
         if fil.endswith(".png"):
             with Image.open(framesDirectory + "/" + fil) as pixelsOfFrame:
@@ -70,11 +73,9 @@ def generateList(w, h, framesDirectory):
                     frames[row][col].append(hexColor)
     return frames
 
-# Writes data to json file
-def saveJson(fileName, data):
-    with open(fileName, 'w') as outfile:
-        json.dump(data, outfile)
-    print ("Saved pixel color data to {}".format(fileName))
+# Writes data to stdout
+def outputJson(data):
+    print (json.dumps(data))
 
 # Converts a tupil (R, G, B) to a hex code
 def rgbToHex(rgb):
@@ -85,6 +86,19 @@ def sort(data):
     convert = lambda text: int(text) if text.isdigit() else text.lower()
     alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ]
     return sorted(data, key=alphanum_key)
+
+# Print to stderr
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+# Creates temp files
+def prep():
+    if not os.path.exists(sys.path[0] + "/temp"):
+        os.makedirs(sys.path[0] + "/temp")
+
+# Removes temp files
+def clean():
+    shutil.rmtree(sys.path[0] + "/temp")
 
 if __name__ == '__main__':
     main()
