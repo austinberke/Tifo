b0b0b48da889ab5e3a007273789855ece113819c
Austin Berke <austinberke@gmail.com>
2018-04-01 01:23:20 -0700

    It works.

--- src/server.js
+++ src/server.js
@@ -38,7 +38,7 @@ function validateLocation(location) {
       return element == location;
     });
     if (col != -1)
-      return [j,col];
+    return [j,col];
   }
   return [];
 }
@@ -59,7 +59,7 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
 
   var check_valid = validateLocation(req.body.location);
-  if (typeof check_valid != "undefined" && check_valid != null && check_valid.length != null && check_valid.length > 0){
+  if (typeof video !== undefined && typeof check_valid != "undefined" && check_valid != null && check_valid.length != null && check_valid.length > 0){
     res.render('display', {location: req.body.location, colors: video[check_valid[0]][check_valid[1]]} );
   }
   else {
@@ -73,75 +73,75 @@ app.get('/admin', (req, res) => {
   console.log(io.sockets.sockets);
   res.render('admin', {
     resWidth: resolution.width, resHeight: resolution.height});
-});
-
-function futureEventTime(seconds) {
-  var t = new Date(Date.now());
-  t.setSeconds(t.getSeconds() + seconds);
-  return t;
-}
-
-app.get('/start', (req, res) => {
-  console.log("Broadcasting start")
-  io.emit('start', {time: futureEventTime(5)});
-});
-
-app.get('/stop', (req, res) => {
-  console.log("Broadcasting stop")
-  io.emit('stop', {time: futureEventTime(0)});
-});
-
-app.post('/processVideo', (req, res) => {
-  var dimensions = getVideoDimensions();
-  var spawn = require('child_process').spawn,
-      py = spawn('python', ['./scripts/videoToJson.py', './assets/video.mp4', dimensions.width, dimensions.height]),
-      string = "";
-
-  py.stdout.on('data', function(data){
-    string += data.toString();
-    console.log(string);
   });
 
-  py.stderr.on('data', function(data){
-    console.log(data.toString());
+  function futureEventTime(seconds) {
+    var t = new Date(Date.now());
+    t.setSeconds(t.getSeconds() + seconds);
+    return t;
+  }
+
+  app.get('/start', (req, res) => {
+    console.log("Broadcasting start")
+    io.emit('start', {time: futureEventTime(5)});
   });
 
-  py.stdout.on('end', function(){
-    console.log(string);
+  app.get('/stop', (req, res) => {
+    console.log("Broadcasting stop")
+    io.emit('stop', {time: futureEventTime(0)});
   });
 
-  res.redirect('/admin');
-});
+  app.post('/processVideo', (req, res) => {
+    var dimensions = getVideoDimensions();
+    var spawn = require('child_process').spawn,
+    py = spawn('python', ['./scripts/videoToJson.py', './assets/video.mp4', dimensions.width, dimensions.height]),
+    string = "";
 
-app.get('/clients', (req, res) => {
-  res.json(Object.keys(io.sockets.sockets));
-});
+    py.stdout.on('data', function(data){
+      string += data.toString();
+      console.log(string);
+    });
 
-app.post('/importMap', (req, res) => {
+    py.stderr.on('data', function(data){
+      console.log(data.toString());
+    });
+
+    py.stdout.on('end', function(){
+      console.log(string);
+    });
+
+    res.redirect('/admin');
+  });
+
+  app.get('/clients', (req, res) => {
+    res.json(Object.keys(io.sockets.sockets));
+  });
+
+  app.post('/importMap', (req, res) => {
 
     const csvFilePath='./assets/map.csv'
     csv({noheader:true})
     .fromFile(csvFilePath)
     .on('csv',(csvRow, rowIndex)=>{ // this func will be called 3 times
-        seat_map.push([]);
-        for (var j = 0; j < csvRow.length; j++){
-            seat_map[rowIndex][j] = csvRow[j];
-        }
+      seat_map.push([]);
+      for (var j = 0; j < csvRow.length; j++){
+        seat_map[rowIndex][j] = csvRow[j];
+      }
     })
     .on('done',(error)=>{
       console.log(seat_map)
     })
     res.redirect('/admin');
-});
+  });
 
-app.post('/setResolution', (req, res) => {
-  resolution.width = req.body.w;
-  resolution.height = req.body.h;
-  res.redirect('/admin');
-});
-// handle timesync requests
-app.use('/timesync', timesyncServer.requestHandler);
+  app.post('/setResolution', (req, res) => {
+    resolution.width = req.body.w;
+    resolution.height = req.body.h;
+    res.redirect('/admin');
+  });
+  // handle timesync requests
+  app.use('/timesync', timesyncServer.requestHandler);
 
-http.listen(PORT, function(){
-  console.log('listening on *:' + PORT);
-});
+  http.listen(PORT, function(){
+    console.log('listening on *:' + PORT);
+  });
