d5a9f8ed5294652fe6e5d361367694dc1806963f
Ali Hashemi <alihashe@usc.edu>
2018-04-29 18:12:05 -0700

    Changes to coming soon page

--- src/comingSoon/config.js
+++ src/comingSoon/config.js
@@ -7,7 +7,7 @@ var background ={
        //  CHOOSE ON OF THIS OPTION
        // OPTION : 'gradient' , 'image' , 'slideShow'
 
-       type   : 'image',
+       type   : 'slideShow',
 
 
 
@@ -21,13 +21,9 @@ var background ={
              slides :
 
                     [
-                      { src: 'https://images.pexels.com/photos/1021808/pexels-photo-1021808.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' },
-                      { src: 'https://images.pexels.com/photos/268487/pexels-photo-268487.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' },
-                      { src: 'https://images.pexels.com/photos/196666/pexels-photo-196666.jpeg?w=940&h=650&auto=compress&cs=tinysrgb' },
-                      { src: 'https://images.pexels.com/photos/196464/pexels-photo-196464.jpeg?w=940&h=650&auto=compress&cs=tinysrgb' },
-                      { src: 'https://images.pexels.com/photos/71104/utah-mountain-biking-bike-biking-71104.jpeg?w=940&h=650&auto=compress&cs=tinysrgb' },
-                      { src: 'https://images.pexels.com/photos/69731/pexels-photo-69731.jpeg?w=940&h=650&auto=compress&cs=tinysrgb' }
-
+                      { src: 'https://images.pexels.com/photos/305831/pexels-photo-305831.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' },
+                      { src: 'https://images.pexels.com/photos/207241/pexels-photo-207241.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' },
+                      { src: 'https://images.pexels.com/photos/933967/pexels-photo-933967.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' }
                     ],
 
                     overlay:'assets/css/overlays/06.png'
@@ -37,12 +33,12 @@ var background ={
        image : {
 
            // choose image for background
-          image_url : "https://images.pexels.com/photos/41257/pexels-photo-41257.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          image_url : "https://images.pexels.com/photos/933967/pexels-photo-933967.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 
         },
 
        //Option : ture ,false "activate or deactivate particle"
-       particle : true
+       particle : false
 
       } ;
 
